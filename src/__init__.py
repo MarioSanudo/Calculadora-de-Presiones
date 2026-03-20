46c7ea52d6ed02bc6f6e9c86cb3731771b4333b3
Mario Sañudo <kartingcroc@gmail.com>
2026-03-20 10:59:53 +0100

    Pequeños cambios en el auth, seguimos!

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,7 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def app_creation(config_class=None):
-
+    
     sentry_sdk.init(
         dsn="https://b480bef0fbff32420ffeaaf8563a3757@o4510783416762368.ingest.de.sentry.io/4511066566361168",
         integrations=[FlaskIntegration()],  traces_sample_rate=0.2
@@ -36,7 +36,7 @@ def app_creation(config_class=None):
 
     @login_manager.user_loader
     def load_user(user_id):  # user_id es el UUID del alternative_id
-        return User.query.filter_by(alternative_id=user_id).first()
+        return User.query.filter_by(alternative_id=user_id).first() #Coge el usuario logueado
 
     # Blueprints
     from src.routes.auth import auth_bp
